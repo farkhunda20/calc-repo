def0d5a99e4a73fb0a1e015d5ce6921f5af17618
farkhunda <farkhundaadel123@gmail.com>
2023-04-07 05:32:39 +0500

    equal functionality added

--- main.js
+++ main.js
@@ -3,11 +3,22 @@
   let buttons = document.querySelectorAll(".btn");
   let clear = document.querySelector(".btn-clear");
   let equal = document.querySelector(".btn-equal");
-
+  //fist functionality
   buttons.forEach(function (button) {
     button.addEventListener("click", function (e) {
       let userInput = e.target.dataset.num;
       screen.value += userInput;
     });
+  });
+
+  //second functionality
+  equal.addEventListener("click", function (e) {
+    if (screen.value === "") {
+      screen.value = "";
+      console.log("equal button clicked");
+    } else {
+      let answer = eval(screen.value);
+      console.log(answer);
+    }
   });
 })();
